8133c07704b89fb1ea88f05e8b43cf697df5cbe0
Elmer Masters <emasters@cali.org>
2014-07-16 14:40:48 -0400

    Update facultyauthorize.php for Drupal 7.

--- lessons/web/authorizefaculty.php
+++ lessons/web/authorizefaculty.php
@@ -5,26 +5,24 @@
 //10/24/2013 SJG This checks with Drupal to make sure user is Faculty or Staff or CALI Staff. 
 // Anyone else gets bounced to home page.
 
-//$referer = $_SERVER['HTTP_REFERER'];
-//$referer_host = parse_url($referer, PHP_URL_HOST);
-//$link_path = $_SERVER['SCRIPT_URL'];
-//$path_pieces = explode('/', $link_path);
-//$lessonid1 = $path_pieces[3];
-//if ($referer_host != $_SERVER['HTTP_HOST'])
-{
-	chdir("/vol/data/acquia-drupal");
-	include_once("./includes/bootstrap.inc");
-	drupal_bootstrap(DRUPAL_BOOTSTRAP_SESSION);
+	chdir("/vol/data/drupal7-cali/");
+	define('DRUPAL_ROOT_DIR','/vol/data/drupal7-cali');
+	// Set the working directory to your Drupal root
+	chdir(DRUPAL_ROOT_DIR);
+	define('DRUPAL_ROOT', getcwd());
+	require_once("./includes/bootstrap.inc");
+	drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 	global $user;
-	$maySeeLessonText = in_array('CALI staff', array_values($user->roles)) || in_array('facstaff', array_values($user->roles));
-	if (!$maySeeLessonText )
+	$roles = $user->roles;
+	//print_r($roles);
+	$maySeeLessonText = in_array('CALI Staff', $roles) || in_array('Facstaff', $roles) || $user->uid == 1;
+	//print_r($maySeeLessonText);
+	if ($maySeeLessonText == FALSE)
 	{
 		// Bounce to home page. 
 		header("Location: http://".$_SERVER['HTTP_HOST']);
 		return;
 	}
 	
-}
-
 echo "<!-- Faculty lesson authorization. -->";
 ?>
