3c9604464d0eafc05a34142db69dff93fbd25253
Sam Goshorn <sam@cali.org>
2016-11-17 09:02:56 -0600

    CSV download for Safari - quick fix
    Hack to get Safari to download CSV from JavaScript directly. File is
    untyped so Mac doesn’t know how to open it requiring teacher to use
    right-click to ‘open with…’ .

--- lessons/web/share/jq/jQuery/exportToCSV.js
+++ lessons/web/share/jq/jQuery/exportToCSV.js
@@ -24,21 +24,27 @@ function exportToCSV(filename, rows) {
             csvFile += processRow(rows[i]);
         }
 
-        var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
-        if (navigator.msSaveBlob) { // IE 10+
-            navigator.msSaveBlob(blob, filename);
+			if (navigator.msSaveBlob) { // IE 10+
+				var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+				navigator.msSaveBlob(blob, filename);
         } else {
             var link = document.createElement("a");
+				var url;
             if (link.download !== undefined) { // feature detection
                 // Browsers that support HTML5 download attribute
-                var url = URL.createObjectURL(blob);
-                link.setAttribute("href", url);
-                link.setAttribute("download", filename);
-                link.style.visibility = 'hidden';
-                document.body.appendChild(link);
-                link.click();
-                document.body.removeChild(link);
-            }
+					var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+					url = URL.createObjectURL(blob);
+				}
+				else{ // Safari
+					url = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csvFile);
+				}
+				link.setAttribute("href", url);
+				link.setAttribute("download", filename);
+				link.setAttribute("target", "_blank");
+				link.style.visibility = 'hidden';
+				document.body.appendChild(link);
+				link.click();
+				document.body.removeChild(link);
         }
     }
 	 
