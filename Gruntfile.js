bfd432c241e1a39f6f58d379b650b220a8fd21ee
themyshkin <artmyshkin@gmail.com>
2018-05-14 12:03:33 -0600

    Adding old images and code and js fix
    Adding pics of current interface and old code so Sam can reference for the dev version he is building. Also cleaned up js and less.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,19 +14,19 @@ module.exports = function(grunt) {
         cwd: 'styles/demos',
         src: '**',
         dest: 'docs/demos/'
-//      },
-//      prodStyles: {
-//        src: 'docs/styles.css',
-//        dest: 'lessons/web/share/jq/cav-ux.css'
+      },
+      prodStyles: {
+        src: 'docs/styles.css',
+        dest: 'lessons/web/share/jq/cav-ux.css'
       }
-    }
-//    clean: ['docs/']
+    },
+    clean: ['docs/']
   });
   grunt.loadNpmTasks('documentjs');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-copy');
-//  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('document', [/*'clean',*/ 'documentjs', 'less', 'copy:demos']);
-//  grunt.registerTask('build', ['less', 'copy:prodStyles']);
+  grunt.registerTask('document', ['clean', 'documentjs', 'less', 'copy:demos']);
+  grunt.registerTask('build', ['less', 'copy:prodStyles']);
 };
