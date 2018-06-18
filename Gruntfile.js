77f0ac3bb12732955bd947560811622c6277121a
Kyle Gifford <kyle@kylegifford.com>
2018-06-18 14:14:46 -0600

    Add Uglify Grunt tasks, include jquery, jquery-ui, jquery-ui.touch-punch with install

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,22 @@ module.exports = function(grunt) {
         options: {
           cleancss: true
         },
-        files: {"docs/styles.css": "styles/styles.less"}
+        files: {
+          "docs/styles.css": "styles/styles.less"
+        }
       },
+    },
+    uglify: {
+      scripts: {
+        files: {
+          "docs/styles.js": [
+            "node_modules/jquery/dist/jquery.min.js",
+            "node_modules/jquery-ui-dist/jquery-ui.min.js",
+            "node_modules/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js",
+            "styles/js/**/*.js"
+          ]
+        }
+      }
     },
     copy: {
       demos: {
@@ -18,15 +32,20 @@ module.exports = function(grunt) {
       prodStyles: {
         src: 'docs/styles.css',
         dest: 'lessons/web/share/jq/cav-ux.css'
+      },
+      prodScripts: {
+        src: 'docs/styles.js',
+        dest: 'lessons/web/share/jq/cav-ux.js'
       }
     },
     clean: ['docs/']
   });
   grunt.loadNpmTasks('documentjs');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('document', ['clean', 'documentjs', 'less', 'copy:demos']);
-  grunt.registerTask('build', ['less', 'copy:prodStyles']);
+  grunt.registerTask('document', ['clean', 'documentjs', 'less', 'uglify', 'copy:demos']);
+  grunt.registerTask('build', ['less', 'copy:prodStyles', 'copy:prodScripts']);
 };
