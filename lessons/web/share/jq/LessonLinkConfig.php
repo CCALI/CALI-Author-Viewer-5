15627fa1b012dabb7e51ef0e5f93cb1da910f4ab
Sam Goshorn <sam@cali.org>
2019-03-13 14:31:28 -0500

    Missing LessonPast config

---
+++ lessons/web/share/jq/LessonLinkConfig.php
@@ -0,0 +1,11 @@
+<?php
+//	10/24/2016 DB/Drupal config for LessonLive/LessonPast 
+//	This is a Helper .php which is called by another .php.
+
+	define('DRUPAL_ROOT_DIR','/vol/data/drupal7-cali');
+	$dbdatabase="";
+	$dbhost="";
+	$dbuser="";
+	$dbpass="";
+
+?>
