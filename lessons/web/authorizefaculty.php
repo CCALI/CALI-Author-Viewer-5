f3ee16a975c3ddb44897f3fa3607146e0002f91a
Sam Goshorn <sam@cali.org>
2026-04-15 15:18:03 -0500

    Update authorizefaculty.php

--- lessons/web/authorizefaculty.php
+++ lessons/web/authorizefaculty.php
@@ -1,11 +1,22 @@
 <?php
-	//	06/01/2009 SJG Insert our Faculty/lesson access authentication code here.
-	// 07/17/2024 SJG If Drupal user is not staff/faculty, eject.
+	// 06/01/2009 SJG Insert our Faculty/lesson access authentication code here.
+	// 04/15/2026 SJG If Drupal user is not CALI/staff/faculty, deny.
 	require "getdrupalinfo.php";
 	if ($userisfacstaff!=1)
-	{
-		header("Location: https://".$_SERVER['HTTP_HOST']);
-		return;
+	{	// 04/15/26 In case of missing session, see if user truly has no facstaff or CALI role.\
+		$numrows=0;
+		if($userid>0){// userid>0 in case of anon users
+			$SQL = "SELECT roles_target_id FROM `user__roles` WHERE entity_id = $userid and roles_target_id in('facstaff','cali_staff')";
+			$result = $umysqli->query($SQL);
+			$numrows=mysqli_num_rows($result);
+		}
+      	if ($numrows==0){
+			//header("Location: http://".$_SERVER['HTTP_HOST']);
+			echo "<p>Only authorized users may see the Faculty View of a lesson.";
+			exit();
+			return;
+		}
+		$userisfacstaff=1;
 	}
-	echo "<!-- Faculty lesson authorization. -->";
+	echo "<!-- Faculty lesson authorization $userid. -->";
 ?>
