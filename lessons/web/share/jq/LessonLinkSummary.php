3ad88db99f6a2d913a283259462fcff49406b7cf
Sam Goshorn <sam@cali.org>
2026-04-23 17:44:09 -0500

    SPL support

--- lessons/web/share/jq/LessonLinkSummary.php
+++ lessons/web/share/jq/LessonLinkSummary.php
@@ -1,5 +1,6 @@
 <?php
-/*	3/26/26 D10 Fixes 
+/*	4/22/26 SPL Support
+	3/26/26 D10 Fixes 
 
 	09/27/2016 A shell for the LessonLive aggregator.
 
@@ -55,7 +56,7 @@
 	
 	$ownerID=0;
 	if ($runID>0)
-	{	//### If runid is specified, lookup LessonRun by runid to find course and lesson id.
+	{	// If runid is specified, lookup LessonRun by runid to find course and lesson id.
 		$SQL="select nid, courseid from LessonRun where runid = $runID limit 1";
 		$q=new QueryMySQLSimple ($SQL);
 		traceSQL($SQL);
@@ -64,24 +65,21 @@
 		$lessonID=$row['nid'];
 	}
 	if ($courseID>0)
-	{	//### Given course id, lookup the owner so that only the Owner can actually load course data.
+	{	// Given course id, lookup the owner so that only the Owner can actually load course data.
 		$SQL="select uid from course where courseid = $courseID limit 1";
 		$q=new QueryMySQLSimple ($SQL);
 		traceSQL($SQL);
 		$row=$q->fetchRow();
 		$ownerID=$row['uid'];
 	}
-	
-	/* 7/24 AP obsolete
 	if ($lessonID>0 && $courseID==0)
-	{	// 12/08/2016 If lesson but no course see if it's creator is this user (assume owner is AP)
-		$node = node_load($lessonID);
-		if($node->uid == $user->uid)
-		{
-        $ownerID = $userID;
-		}
+	{	// If lesson but no course see if the lesson author (node user) is current user) - an SPL lesson run without LessonLink.
+		$SQL="select uid from node_field_data where nid=$lessonID";// Owner of lesson node (SPL author will be user).
+		$q=new QueryMySQLSimple ($SQL);
+		traceSQL($SQL);
+		$row=$q->fetchRow();
+		$ownerID=$row['uid'];
 	}
-	*/
 	
 	//var_dump(array($node->uid,$lessonID,$courseID,$userID,$user->uid,$ownerID));
 	
@@ -92,7 +90,7 @@
 	}
 	else
 	if (($userID==0) || (($userID!=$ownerID)&&(!$userisstaff)))
-	{	// Anonymous user or not owner or CALI staff.
+	{	// Anonymous user or not (owner of course or SPL lesson) or CALI staff.
 		echo json_error(
 			"Only LessonLink owner of this course may access this data"
 			//"Only LessonLink owner of course $courseID may access this data: $userID<>$ownerID"
